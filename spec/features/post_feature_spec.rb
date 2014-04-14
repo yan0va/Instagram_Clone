3389e7e40ce0aba04b2bb40c0f1fd73eea9b2759
Anna <anna.yanova@gmail.com>
2014-04-14 19:32:10 +0100

    Generated resource Post

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -4,7 +4,7 @@ describe 'posts index page' do
   context 'no posts' do
     it 'shows a message' do
       visit '/posts'
-      expect(page).to have_content 'No posts yet'
+      expect(page).to have_content 'No posts added'
     end
   end
 
@@ -25,7 +25,7 @@ describe 'posts index page' do
         visit '/posts/new'
         click_button 'Create Post'
 
-        expect(page).to have_content 'errors'
+        expect(page).to have_content 'error'
       end
     end
   end
